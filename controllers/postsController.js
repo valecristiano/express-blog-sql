784eda04a0ec286606c158da882ce8dc74b14eb8
valecristiano <valeriamdcristiano@gmail.com>
2026-03-13 18:51:06 +0300

    Milestone 2

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -4,6 +4,17 @@ const connection = require("../database/connection");
 function index(req, res) {
   //prova errore 500:
   // console.log(a.b);
+
+  const sql = `SELECT * FROM posts`;
+
+  connection.query(sql, (err, results) => {
+    if (err)
+      return res.status(500).json({
+        message: "Internal server error",
+        success: false,
+      });
+    res.json(results);
+  });
 }
 
 module.exports = { index };
